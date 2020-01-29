490ead21b6496aa3e6c0e8e1fbc290370258b38a
Mihai Tirtara <mihai.tirtara@gmail.com>
2020-01-29 11:57:17 +0100

    turn rgb matrix into intensity matrix

--- asciiArt.py
+++ asciiArt.py
@@ -1,3 +1,22 @@
 from PIL import Image
+import numpy as np
 im = Image.open("ascii-pineapple.jpg")
-print(im.size)
+ASCII_CHARS = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+
+def get_pixelMatrix(img) :
+    pixels = list(img.getdata())
+    return [pixels[i:i+img.width] for i in range(0,len(pixels),img.width)]
+
+
+def get_brightnessMatrix(matrix):
+    new_matrix = []
+    for x in range(len(matrix)):
+        result = [sum(y)/ len(y) for y in matrix[x]]
+    new_matrix.append(result)
+    return new_matrix
+
+pixels = get_pixelMatrix(im)
+
+brightness_pixels = get_brightnessMatrix(pixels)
+
+print(brightness_pixels)
